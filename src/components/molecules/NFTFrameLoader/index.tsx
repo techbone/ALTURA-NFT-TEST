2428c593b873f456960d61be4b08bd52eac5c430
Musa Muhammad <musaawwaletudaye@gmail.com>
2024-10-07 15:36:54 +0100

    feat: fixed nav component

--- src/components/molecules/NFTFrameLoader/index.tsx
+++ src/components/molecules/NFTFrameLoader/index.tsx
@@ -1,7 +1,7 @@
 const NFTSkeletonLoader = () => {
   return (
     <div className="bg-white shadow-md flex flex-col rounded-t-xl h-[22rem]">
-      <div className="h-[80%] w-full rounded-t-xl bg-gray-300 animate-pulse"></div>
+      <div className="h-[85%] w-full rounded-t-xl bg-gray-300 animate-pulse"></div>
       <div className="h-[20%] flex flex-col justify-between w-full py-2 px-3">
         <div className="w-full flex justify-between gap-x-4">
           <span className="h-5 w-[80%] rounded bg-gray-300 animate-pulse"></span>
